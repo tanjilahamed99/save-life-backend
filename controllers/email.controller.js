ccdcbf10595266474eed7d2d95ae610995aa516f
AbuBokorprog <abubokor1066@gmail.com>
2025-04-05 15:47:20 +0600

    Email template issue fixed.

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,59 +1,75 @@
-import sendEmail from "../lib/email/sendEmail.js";
-import { OrderModel } from "../models/order.model.js";
-import { UserModel } from "../models/user.model.js";
+import Email from '../lib/email/emai.js';
+import { OrderModel } from '../models/order.model.js';
+import { UserModel } from '../models/user.model.js';
+import { paymentRequestEmailTemplate } from '../static/email/paymentRequestEmailTemplate.js';
 
 export const paymentRequest = async (req, res) => {
-	const { orderId, pay_amount, expiry_date, payment_url } = req.body;
+  const { orderId, pay_amount, expiry_date, payment_url } = req.body;
 
-	// Check if order ID and message is provided
-	if (!orderId) {
-		return res
-			.status(201)
-			.send({ status: false, message: "Order ID is required" });
-	}
+  // Check if order ID and message is provided
+  if (!orderId) {
+    return res
+      .status(201)
+      .send({ status: false, message: 'Order ID is required' });
+  }
 
-	// // Check if order exists
-	const order = await OrderModel.findById(orderId);
+  // // Check if order exists
+  const order = await OrderModel.findById(orderId);
 
-	if (!order) {
-		return res.status(201).send({ status: false, message: "Order not found" });
-	}
+  if (!order) {
+    return res.status(201).send({ status: false, message: 'Order not found' });
+  }
 
-	const { email } = await UserModel.findById(order.user);
+  const user = await UserModel.findById(order.user);
 
-	if (!email) {
-		return res.status(201).send({ status: false, message: "User not found" });
-	}
-	await sendEmail({
-		subject: "Payment Request benzobestellen.net",
-		template_name: "payment_request_template",
-		pay_amount,
-		payment_url,
-		expiry_date,
-		email,
-	});
+  if (!user) {
+    return res.status(201).send({ status: false, message: 'User not found' });
+  }
+  // await sendEmail({
+  // 	subject: "Payment Request benzobestellen.net",
+  // 	template_name: "payment_request_template",
+  // 	pay_amount,
+  // 	payment_url,
+  // 	expiry_date,
+  // 	email,
+  // });
 
-	res.send({ status: true, message: "Payment email sent" });
+  const htmlContent = await paymentRequestEmailTemplate({
+    expiry_date,
+    pay_amount,
+    payment_url,
+  });
+
+  try {
+    await new Email(user).sendEmailTemplate(
+      htmlContent,
+      'Payment Request benzobestellen.net'
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.send({ status: true, message: 'Payment email sent' });
 };
 
 export const marketingCampaign = async (req, res) => {
-	const { name, email } = req.body;
+  const { name, email } = req.body;
 
-	if (!email) {
-		return res
-			.status(201)
-			.send({ status: false, message: "Email is required" });
-	}
+  if (!email) {
+    return res
+      .status(201)
+      .send({ status: false, message: 'Email is required' });
+  }
 
-	await sendEmail({
-		subject: `${name}, , Exclusieve Korting! 10% Korting op Uw Eerste Bestelling!`,
-		template_name: "marketing_campaign_template",
-		email,
-		name,
-	});
+  await sendEmail({
+    subject: `${name}, , Exclusieve Korting! 10% Korting op Uw Eerste Bestelling!`,
+    template_name: 'marketing_campaign_template',
+    email,
+    name,
+  });
 
-	res.send({
-		status: true,
-		message: "Marketing email sent",
-	});
+  res.send({
+    status: true,
+    message: 'Marketing email sent',
+  });
 };
