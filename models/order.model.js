c865aa13f2480a7f342a8a29e292d072511cd75d
me-sajib <me.mrsajib@gmail.com>
2025-04-19 11:37:19 +0600

    fix

--- models/order.model.js
+++ models/order.model.js
@@ -1,40 +1,40 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-  firstName: { type: String, required: false },
-  lastName: { type: String, required: false },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  address: { type: String, required: true },
-  city: { type: String, required: true },
-  postalCode: { type: String, required: true },
-  country: { type: String, required: true },
-  paymentMethod: { type: String, required: true },
-  paymentStatus: { type: String, default: 'pending' },
-  orderStatus: { type: String, default: 'pending' },
-  user: {
-    _id: { type: mongoose.Schema.Types.ObjectId },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    role: { type: String, required: true },
-  },
-  items: [
-    {
-      name: { type: String, required: true },
-      price: { type: Number, required: true },
-      quantity: { type: Number, required: true },
-      imageUrl: { type: String, required: true },
-    },
-  ],
-  shipping: { type: Number, required: true },
-  subtotal: { type: Number, required: true },
-  totalAmount: { type: Number, required: true },
-  site: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+	firstName: { type: String, required: false },
+	lastName: { type: String, required: false },
+	email: { type: String, required: true },
+	phone: { type: String, required: true },
+	address: { type: String, required: true },
+	city: { type: String, required: true },
+	postalCode: { type: String, required: true },
+	country: { type: String, required: true },
+	paymentMethod: { type: String, required: true },
+	paymentStatus: { type: String, default: "pending" },
+	orderStatus: { type: String, default: "pending" },
+	user: {
+		_id: { type: mongoose.Schema.Types.ObjectId },
+		name: { type: String, required: true },
+		email: { type: String, required: true },
+		role: { type: String, required: true },
+	},
+	items: [
+		{
+			name: { type: String, required: true },
+			price: { type: Number, required: true },
+			quantity: { type: Number, required: true },
+			imageUrl: { type: String },
+		},
+	],
+	shipping: { type: Number, required: true },
+	subtotal: { type: Number, required: true },
+	totalAmount: { type: Number, required: true },
+	site: { type: String, required: true },
+	createdAt: { type: Date, default: Date.now },
 });
 
-orderSchema.pre('save', function (next) {
-  this.updatedAt = new Date();
-  next();
+orderSchema.pre("save", function (next) {
+	this.updatedAt = new Date();
+	next();
 });
-export const OrderModel = mongoose.model('Order', orderSchema);
+export const OrderModel = mongoose.model("Order", orderSchema);
