e3189eb79cf14520c903a2b56eb6b01f722b5799
me-sajib <me.mrsajib@gmail.com>
2025-04-27 11:28:34 +0600

    fix

--- models/user.model.js
+++ models/user.model.js
@@ -19,6 +19,11 @@ const userSchema = new mongoose.Schema({
 		enum: ["admin", "customer"],
 		default: "customer",
 	},
+	status: {
+		type: String,
+		enum: ["active", "inactive"],
+		default: "active",
+	},
 	phone: String,
 	address: String,
 	totalOrders: {
